7616a460facf8c04c491668307184589037e9d4d
rixx <rike.kunze@gmail.com>
2013-07-29 10:07:29 +0200

    implemented status function

--- tempus.py
+++ tempus.py
@@ -20,6 +20,39 @@ def writelog(status):
 
     logfile.close()
 
+def status():
+    logfile = open(logfile_path, 'r')
+    logreader = csv.reader(logfile)
+
+    sum_seconds = 0
+    status = 0
+    lasttimestamp = 0
+
+    for entry in logreader:
+        
+        entry = [int(i) for i in entry]
+        if entry[0] == 1:
+            
+            if status == 0:
+                status = 1
+                lasttimestamp = entry[1]
+
+        if entry[0] == 0:
+
+            if status == 1:
+                status = 0
+                sum_seconds += entry[1] - lasttimestamp
+
+
+    if (status == 1):
+        sum_seconds += math.trunc(time.time()) - lasttimestamp
+
+    print("You have worked "+ str(math.trunc(sum_seconds/60)) +" minutes today.")
+
+    if (status == 1):
+        print("Also, you should be working right now!")
+
+
 
 
 if __name__ == "__main__":
