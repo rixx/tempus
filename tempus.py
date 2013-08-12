9f0de49cb837d29f326ad6f2b640ebaa51c85793
rixx <rike.kunze@gmail.com>
2013-08-12 17:10:08 +0200

    added logger

--- tempus.py
+++ tempus.py
@@ -5,7 +5,9 @@ import sys
 import math
 import time
 import csv
+import logging
 
+timefile_path = os.path.expanduser('~') + '/.tempus/time'
 logfile_path = os.path.expanduser('~') + '/.tempus/log'
 
 def start():
@@ -15,19 +17,22 @@ def stop():
     writelog(0)
 
 def writelog(status):
-    logfile = open(logfile_path, 'a+')
+    logfile = open(timefile_path, 'a+')
     logwriter = csv.writer(logfile)
 
     logwriter.writerow([status, math.trunc(time.time())])
+    logger.debug("Wrote "+str(status)+" to "+timefile_path+".")
 
     logfile.close()
 
 def clear():
     try:
-        os.remove(logfile_path)
+        os.remove(timefile_path)
         print("Log has been cleared.")
+        logger.info("Log has been cleared")
     except FileNotFoundError:
-        print("No logfile at "+logfile_path);
+        print("No file at "+timefile_path);
+        logger.info("No file at "+timefile_path+".")
 
 def status():
     
@@ -36,7 +41,8 @@ def status():
         logreader = csv.reader(logfile)
 
     except FileNotFoundError:
-        print("You haven't even started yet, or the logfile at "+logfile_path+" has gone missing.");
+        print("You haven't even started yet, or the file at "+timefile_path+" has gone missing.");
+        logger.error("File at "+timefile_path+" does not exist.")
         return;
 
     sum_seconds = 0
@@ -66,9 +72,11 @@ def status():
 
     if (status == 1):
         print("Also, you should be working right now!")
+    logger.info("Printed status.")
 
 
-
+logging.basicConfig(filename=logfile_path, level=logging.DEBUG, format="%(asctime)s - %(levelname)s: %(message)s")
+logger = logging.getLogger("tempus")
 
 if __name__ == "__main__":
 
@@ -76,7 +84,6 @@ if __name__ == "__main__":
 
         if "start" == sys.argv[1]:
             start()
-
 
         if ("stop" == sys.argv[1]) or ("pause" == sys.argv[1]):
             stop()
