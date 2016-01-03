c33a781ed3de956ac2e0a2cc30e3913d433220c4
Tobias Kunze <tobias.kunze@ax-semantics.com>
2016-01-03 22:14:47 +0100

    slight changes in urls and output

--- tempus/timetracking/urls.py
+++ tempus/timetracking/urls.py
@@ -3,10 +3,19 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # /time/
+    # /t/
+    # shows all categories (work/fun/uni) and a quickstart menu
     url(r'^$', views.index, name='index'),
-    # /time/work/
-    url(r'^(?P<category>[a-zA-Z0-9]+)/$', views.projects, name='projects'),
-    # /time/overview
-    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+
+    # /t/results
+    url(r'^results/$', views.results, name='results'),
+
+    # /t/work/
+    # shows all projects and entries for a category
+    url(r'^(?P<category>[a-zA-Z0-9]+)/$', views.category, name='category'),
+
+    # /t/work/redo_things
+    # shows project redo_things from category work
+    url(r'^(?P<category>[a-zA-Z0-9]+)/(?P<project>[a-zA-Z0-9]+)/$', views.project, name='project'),
+
 ]
--- tempus/timetracking/views.py
+++ tempus/timetracking/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Category
 
 
 def index(request):
-    return HttpResponse('This site will present you the main categories and your current status.')
+    template = loader.get_template('t/index.html')
+    context = {'all_categories': Category.objects.all()}
+    return HttpResponse(template.render(context, request))
 
-def projects(request, category):
+def category(request, category):
     return HttpResponse('This site will show you projects of category {}.'.format(category))
 
-def dashboard(request):
+def project(request, category, project):
+    return HttpResponse('This site will show you project {} of category {}.'.format(project, category))
+
+def results(request):
     return HttpResponse('Here you will be able to look at your past working habits.')
