cc54b7583d5b5d7efa5b701b3b411d0e2f0fbaf7
Tobias Kunze <tobias.kunze@ax-semantics.com>
2016-01-03 22:13:03 +0100

    register models for admin use

--- tempus/timetracking/admin.py
+++ tempus/timetracking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Project)
+admin.site.register(models.Entry)
