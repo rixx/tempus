c6a62d29d7150c1a00ecb3b5e13dbf92b98ebc98
Tobias Kunze <tobias.kunze@ax-semantics.com>
2016-01-08 19:30:10 +0100

    use bootstrap 4 and the correct static files

--- tempus/tempus/urls.py
+++ tempus/tempus/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^t/', include('timetracking.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
