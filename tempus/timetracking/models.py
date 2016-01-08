379a9f20d8cdc0d22bfd954232c383fd47a27da0
Tobias Kunze <tobias.kunze@ax-semantics.com>
2016-01-08 20:38:47 +0100

    better string representation

--- tempus/timetracking/models.py
+++ tempus/timetracking/models.py
@@ -3,15 +3,27 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.category_name
+
 class Tag(models.Model):
     tag_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.tag_name
 
 class Project(models.Model):
     project_name = models.CharField(max_length=200)
     category = models.ForeignKey(Category)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.project_name
 
 class Entry(models.Model):
     project = models.ForeignKey(Project)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+
+    def __str__(self):
+        return "Entry for {}, {} ago".format(self.project, datetime.datetime.now() - self.end_time)
